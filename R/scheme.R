a2001ce6d1b426bd8cdb08cb46a731f732850854
painterly <painterlyanalytics@gmail.com>
2015-05-23 21:19:56 -0500

    added docs

--- R/scheme.R
+++ R/scheme.R
@@ -1,7 +1,7 @@
 #' dottedpaiR
 #' Scheme functions in R
 #'
-#' @name Scheme
+#' @name scheme
 #'
 #' @param ls list
 #' @param f function
@@ -14,54 +14,65 @@
 #' cons(3,cons(4,nil))
 #' cdr(cons(3,cons(4,nil)))
 
-
+#' @export
 nil <- pairlist()
 
+#' @export
 null_huh <- function(x) {
   is.null(x)
 }
 
+#' @export
 pair_huh <- function(x) {
   is.pairlist(x) && !is.null(x)
 }
 
+#' @export
 cons <- function(a,d) {
   pairlist(a,d)
 }
 
+#' @export
 car <- function(ls) {
   ls[[1]]
 }
 
+#' @export
 cdr <- function(ls) {
   ls[[2]]
 }
 
+#' @export
 cadr <- function(ls) {
   car(cdr(ls))
 }
 
+#' @export
 cdar <- function(ls) {
   cdr(car(ls))
 }
 
+#' @export
 caddr <- function(ls) {
   car(cdr(cdr(ls)))
 }
 
+#' @export
 caadr <- function(ls) {
   car(car(cdr(ls)))
 }
 
+#' @export
 cddr <- function(ls) {
   (cdr(cdr(ls)))
 }
 
+#' @export
 cadar <- function(ls) {
   car(cdr(car (ls)))
 }
 
-
+#' @export
 list_length <- function(ls) {
   if (null_huh(ls))
     { 0 }
@@ -69,6 +80,7 @@ list_length <- function(ls) {
     { 1 + list_length(cdr(ls)) }
 }
 
+#' @export
 list_append <- function(l,s) {
   if (null_huh(l))
     { s }
@@ -76,6 +88,7 @@ list_append <- function(l,s) {
     { cons(car(l), list_append(cdr(l), s)) }
 }
 
+#' @export
 list_map <- function(f,ls) {
   if (null_huh(ls))
     { nil }
